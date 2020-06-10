1c3e00765b5fb592bd20766a869ed6a9ec735aa5
OpenTICHILE <asandovalster@gmail.com>
2020-06-10 08:20:50 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,9 +3,9 @@ provider "aws" {
   region = var.aws.region
 
   # AWS_ACCESS_KEY_ID
-  access_key = "AKIAJVBZCK3RWGHLAKGQ"
+  access_key = "*******************"
   # AWS_SECRET_ACCESS_KEY
-  secret_key = "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"
+  secret_key = "****************"
 
 }
 
